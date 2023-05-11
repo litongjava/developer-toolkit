7440601691e067f4e53fb506d2232bc58cc8dabb
litongjava <litongjava@qq.com>
2023-05-11 17:03:25 +0800

    增加内容合并为1行

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <ToolKits/>
+  </div>
+</template>
+
+<script>
+import ToolKits from '@/components/ToolKits.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToolKits
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ToolKits/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToolKits from '@/components/ToolKits.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    ToolKits
+  }
+}
+</script>
