5a76c23286d9fd13f411b00b9ac71a53e8bfe655
litongjava <litongjava@qq.com>
2023-05-13 18:07:19 +0800

    add underLineToCame and cameToUnderLine

--- src/components/ToolKits.vue
+++ src/components/ToolKits.vue
@@ -2,42 +2,42 @@
   <div class="app-container">
     <div>
       <el-row>
-        <el-button type="primary" @click="stringMargeToOnLine">合并内容为一行</el-button>
+        <el-button type="primary" @click="output = toolkit.stringMargeToOnLine(input)">合并内容为一行
+        </el-button>
+        <el-button type="primary" @click="output=toolkit.underLineToCame(input)">下划线转驼峰
+        </el-button>
+        <el-button type="primary" @click="output=toolkit.cameToUnderline(input)">驼峰转下划线
+        </el-button>
       </el-row>
-
     </div>
 
     <el-input
         type="textarea"
         :autosize="{ minRows: 5, maxRows: 5}"
         placeholder="输入内容"
-        v-model="inputString">
+        v-model="input">
     </el-input>
     <div style="margin: 20px 0;">result:</div>
     <el-input
         type="textarea"
         :autosize="{ minRows: 5, maxRows: 5}"
         placeholder="输出内容"
-        v-model="outputString">
+        v-model="output">
     </el-input>
   </div>
 </template>
 
 <script>
+import toolkit from '../utils/toolkit';
+
 export default {
   name: 'ToolKits',
   data() {
     return {
-      inputString: '',
-      outputString: ''
+      input: '',
+      output: '',
+      toolkit: toolkit
     }
   },
-  methods:{
-    stringMargeToOnLine(){
-      // 使用正则表达式 /[\n\r\s]+/g 来匹配任何连续的换行和空格。[\n\r\s] 匹配换行符、回车符和空格，+ 匹配一个或多个连续的匹配项，g 表示全局匹配，即匹配整个字符串中的所有匹配项。最后，我们使用字符串的 replace 方法将匹配项替换为空字符串，从而删除它们
-      this.outputString = this.inputString.replace(/[\n\r\s]+/g, "");
-      //this.outputString= JSON.stringify(this.inputString).replace(/,/g, ",");
-    }
-  }
 }
 </script>
---
+++ src/utils/toolkit.js
@@ -0,0 +1,23 @@
+const toolkit = {
+  stringMargeToOnLine(input) {
+    // 使用正则表达式 /[\n\r\s]+/g 来匹配任何连续的换行和空格。[\n\r\s] 匹配换行符、回车符和空格，+ 匹配一个或多个连续的匹配项，g 表示全局匹配，即匹配整个字符串中的所有匹配项。最后，我们使用字符串的 replace 方法将匹配项替换为空字符串，从而删除它们
+    return input.replace(/[\n\r\s]+/g, "");
+    //this.outputString= JSON.stringify(this.inputString).replace(/,/g, ",");
+  },
+  underLineToCame(input) {
+    // 使用正则表达式匹配下划线并且后面的字符
+    return input.replace(/_([a-z])/g, function (match, letter) {
+      // 将匹配到的字符替换成大写字母，并去掉下划线
+      return letter.toUpperCase();
+    });
+  },
+  cameToUnderline(input) {
+    // 使用正则表达式匹配大写字母
+    return input.replace(/([A-Z])/g, function (match, letter) {
+      // 将匹配到的大写字母替换成下划线加小写字母
+      return "_" + letter.toLowerCase();
+    });
+  }
+}
+
+export default toolkit;
